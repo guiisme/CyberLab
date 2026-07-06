dd5dbb33f212cfbcb8bcfaa73c6410ff44ccdb8e
guiisme84 <gui.gccintra@gmail.com>
2026-07-06 01:55:24 -0300

    feat(application): add LabRepositoryProtocol

---
+++ src/cyberlab/application/interfaces/lab_repository_protocol.py
@@ -0,0 +1,10 @@
+from typing import Protocol
+
+from cyberlab.domain.models.lab import Lab
+
+
+class LabRepositoryProtocol(Protocol):
+    """Provides access to available CyberLab laboratories."""
+
+    def list(self) -> tuple[Lab, ...]:
+        """Return all available laboratories."""
